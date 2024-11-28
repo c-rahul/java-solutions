f267bea5912480b998362c69bb885dc4f6ad49d3
Rahul Chattopadhyay <rahul.yay@gmail.com>
2024-11-28 21:02:17 +0530

    Pairs

--- src/main/java/mycode/BinarySearch.java
+++ src/main/java/mycode/BinarySearch.java
@@ -1,7 +1,16 @@
 package mycode;
 
+import java.util.Random;
+
 public class BinarySearch {
 
+    /**
+     * Performs a binary search on a sorted array to find a specified number.
+     *
+     * @param arr the sorted array of integers to search in
+     * @param numberToSearch the integer value to search for
+     * @return true if the number is found in the array, false otherwise
+     */
     static boolean binarySearch(int[] arr, int numberToSearch) {
 
         int left = 0;
@@ -23,4 +32,8 @@ public class BinarySearch {
         return false;
     }
 
+    public static void main(String[] args) {
+        int num = new Random().nextInt(20);
+        System.out.println("num: " + num + " found: " + BinarySearch.binarySearch(new int[] {0, 1, 3, 5, 7, 8, 9, 11}, num));
+    }
 }
