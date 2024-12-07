70cb3d40943b8c486fd8e948ad03ede3a9fe1739
Rahul Chattopadhyay <rahul.yay@gmail.com>
2024-12-07 13:33:41 +0530

    Sherlock Valid String

---
+++ src/main/java/mycode/SherlockValidString.java
@@ -0,0 +1,41 @@
+package mycode;
+
+import java.util.*;
+
+public class SherlockValidString {
+    public static void main(String[] args) {
+        System.out.println(isValid("ibfdgaeadiaefgbhbdghhhbgdfgeiccbiehhfcggchgghadhdhagfbahhddgghbdehidbibaeaagaeeigffcebfbaieggabcfbiiedcabfihchdfabifahcbhagccbdfifhghcadfiadeeaheeddddiecaicbgigccageicehfdhdgafaddhffadigfhhcaedcedecafeacbdacgfgfeeibgaiffdehigebhhehiaahfidibccdcdagifgaihacihadecgifihbebffebdfbchbgigeccahgihbcbcaggebaaafgfedbfgagfediddghdgbgehhhifhgcedechahidcbchebheihaadbbbiaiccededchdagfhccfdefigfibifabeiaccghcegfbcghaefifbachebaacbhbfgfddeceababbacgffbagidebeadfihaefefegbghgddbbgddeehgfbhafbccidebgehifafgbghafacgfdccgifdcbbbidfifhdaibgigebigaedeaaiadegfefbhacgddhchgcbgcaeaieiegiffchbgbebgbehbbfcebciiagacaiechdigbgbghefcahgbhfibhedaeeiffebdiabcifgccdefabccdghehfibfiifdaicfedagahhdcbhbicdgibgcedieihcichadgchgbdcdagaihebbabhibcihicadgadfcihdheefbhffiageddhgahaidfdhhdbgciiaciegchiiebfbcbhaeagccfhbfhaddagnfieihghfbaggiffbbfbecgaiiidccdceadbbdfgigibgcgchafccdchgifdeieicbaididhfcfdedbhaadedfageigfdehgcdaecaebebebfcieaecfagfdieaefdiedbcadchabhebgehiidfcgahcdhcdhgchhiiheffiifeegcfdgbdeffhgeghdfhbfbifgidcafbfcd"));
+        System.out.println(isValid("aabbccddeefghi"));
+    }
+
+    private static String isValid(String s) {
+        if (s.length() == 1) {
+            return "YES";
+        }
+        Map<Character, Integer> freqMap = new HashMap<>();
+
+        for (char c : s.toCharArray()) {
+            freqMap.put(c, freqMap.getOrDefault(c, 0) + 1);
+        }
+        int freq = freqMap.get(s.charAt(0));
+        boolean reduced = false;
+        for (char c : freqMap.keySet()) {
+            int currFreq = freqMap.get(c);
+            if (currFreq == freq) {
+                continue;
+            }
+            if (!reduced) {
+                if (Math.abs(currFreq - freq) == 1){
+                    reduced = true;
+                } else if (currFreq == 1) {
+                    reduced = true;
+                } else {
+                    return "NO";
+                }
+            } else {
+                return "NO";
+            }
+        }
+        return "YES";
+    }
+}
